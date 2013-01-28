b5161f75a2e18cee4567273b30629a19cda88127
Igor Mishchenko <igormishchenko@Mac-mini-Igor.local>
2013-01-28 13:24:59 +0400

    Initial Commit

---
+++ IMFinance/IMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IMAppDelegate.h
+//  IMFinance
+//
+//  Created by Igor Mishchenko on 28.01.13.
+//  Copyright (c) 2013 Igor Mishchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

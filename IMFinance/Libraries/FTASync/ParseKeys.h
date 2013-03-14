ec7b8a5ece766094e3aea25e0e2dda7003ec7b50
Игорь Мищенко <imischenko@itrack.ru>
2013-03-15 01:19:49 +0400

    installed FTASync library

---
+++ IMFinance/Libraries/FTASync/ParseKeys.h
@@ -0,0 +1,13 @@
+//
+//  ParseKeys.h
+//  IMFinance
+//
+//  Created by Игорь Мищенко on 15.03.13.
+//  Copyright (c) 2013 Igor Mishchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseKeys : NSObject
+
+@end

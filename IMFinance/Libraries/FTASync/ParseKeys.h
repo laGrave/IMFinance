06f69600b0752734741bdd7672fbb99ad434109b
Игорь Мищенко <imischenko@itrack.ru>
2013-03-17 13:15:26 +0400

    Revert "parse keys"
    This reverts commit 9ede18bbc169b0a9258336ff8fe56398bffcc325.

--- IMFinance/Libraries/FTASync/ParseKeys.h
+++ IMFinance/Libraries/FTASync/ParseKeys.h
@@ -8,9 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-#define kParseAppId @"pIpS2OPgLbzbt6M0DmQ5A2RFk0VjCL8kRMsvTlPl"
-#define kParseClientKey @"CcPTCbuGwdJwlxK47H1Xuf8Wu4UVKQs4cdhWiYix"
-
 @interface ParseKeys : NSObject
 
 @end
